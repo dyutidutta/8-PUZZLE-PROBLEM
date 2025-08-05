e44fe7c4aecb77361cd0ad442faaf12c7a806cac
dyutidutta <159049745+dyutidutta@users.noreply.github.com>
2025-08-05 10:01:53 +0530

    Add files via upload

---
+++ 8_PUZZLE_DFS.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <set>
+#include <list>
+using namespace std;
+
+using Matrix = vector<vector<int>>;
+
+bool isSame(Matrix x, Matrix y)
+{
+    return x == y;
+}
+
+list<Matrix> genmove(Matrix state)
+{
+    list<Matrix> nextStates;
+    int a, b;
+
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (state[i][j] == 0)
+            {
+                a = i;
+                b = j;
+            }
+
+    if (a - 1 >= 0)
+    {
+        Matrix A1 = state;
+        swap(A1[a][b], A1[a - 1][b]);
+        nextStates.push_back(A1);
+    }
+
+    if (a + 1 < 3)
+    {
+        Matrix A2 = state;
+        swap(A2[a][b], A2[a + 1][b]);
+        nextStates.push_back(A2);
+    }
+
+    if (b - 1 >= 0)
+    {
+        Matrix A3 = state;
+        swap(A3[a][b], A3[a][b - 1]);
+        nextStates.push_back(A3);
+    }
+
+    if (b + 1 < 3)
+    {
+        Matrix A4 = state;
+        swap(A4[a][b], A4[a][b + 1]);
+        nextStates.push_back(A4);
+    }
+
+    return nextStates;
+}
+
+string matrixToString(Matrix mat)
+{
+    string s;
+    for (auto row : mat)
+        for (int val : row)
+            s += to_string(val);
+    return s;
+}
+
+void dfs(Matrix prob, Matrix sol)
+{
+    stack<Matrix> s;
+    set<string> visited;
+
+    s.push(prob);
+    visited.insert(matrixToString(prob));
+
+    while (!s.empty())
+    {
+        Matrix A = s.top();
+        s.pop();
+
+        if (isSame(A, sol))
+        {
+            cout << "Success!" << endl;
+            return;
+        }
+
+        list<Matrix> states = genmove(A);
+        for (auto state : states)
+        {
+            string rep = matrixToString(state);
+            if (visited.find(rep) == visited.end())
+            {
+                visited.insert(rep);
+                s.push(state);
+            }
+        }
+    }
+
+    cout << "Failed!" << endl;
+}
+
+int main()
+{
+    Matrix prob = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 0, 8}};
+
+    Matrix sol = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0}};
+
+    dfs(prob, sol);
+
+    return 0;
+}
